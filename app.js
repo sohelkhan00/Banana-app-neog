961c0acad6f236cc07a76d7576747256b9b213e0
Mohammad Sohel Khan <sohelkhan0378@gmail.com>
2020-11-29 03:19:57 +0530

    style: style done as per design

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const btnTranslate = document.querySelector("#btn-translate");
-console.log(btnTranslate);
+const textInput = document.querySelector("[name = text-input]");
+const textOutput = document.querySelector("#text-output");
+const serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function urlCreater(text) {
+  return serverUrl + "?text=" + text;
+}
+
 btnTranslate.addEventListener("click", () => {
-  console.log("Clicked");
+  const inputText = textInput.value;
+  fetch(urlCreater(inputText))
+    .then((response) => response.json())
+    .then((json) => (textOutput.innerHTML = json.contents.translated))
+    .catch((err) => alert(err));
 });
